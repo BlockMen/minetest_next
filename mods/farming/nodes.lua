3bac0a0e5168b385c8b62f15b8c46897f40fbd71
webdesigner97 <Christian_D_97@gmx.de>
2014-04-17 13:33:28 +0200

    clean up files

--- mods/farming/nodes.lua
+++ mods/farming/nodes.lua
@@ -51,3 +51,22 @@ minetest.register_abm({
 		end
 	end,
 })
+
+
+for i = 1, 5 do		
+	minetest.override_item("default:grass_"..i, {drop = {
+		max_items = 1,
+		items = {
+			{items = {'farming:seed_wheat'},rarity = 5},
+			{items = {'default:grass_1'}},
+		}
+	}})
+end
+	
+minetest.override_item("default:junglegrass", {drop = {
+	max_items = 1,
+	items = {
+		{items = {'farming:seed_cotton'},rarity = 8},
+		{items = {'default:junglegrass'}},
+	}
+}})
