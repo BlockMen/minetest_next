a3966eaf6931001b3ccb793f22e331efb9eb4baa
LNJ <lnj@jahnson.de>
2015-11-08 19:40:13 +0100

    Remove nodes (group:dissolve) in liquids

--- mods/default/environment.lua
+++ mods/default/environment.lua
@@ -34,6 +34,28 @@ minetest.register_abm({
 	end,
 })
 
+--
+-- Remove nodes in liquids
+--
+
+minetest.register_abm({
+    nodenames = {"group:dissolve"},
+    neighbors = {"group:liquid"},
+    interval = 2,
+    chance = 5,
+    action = function(pos, node)
+        local above = {x=pos.x, y=pos.y+1, z=pos.z}
+        local name = minetest.get_node(above).name
+        local nodedef = minetest.registered_nodes[name]
+        if nodedef and nodedef.liquidtype then
+            if liquid_finite then
+                minetest.set_node(pos, {name = "air"})
+            else
+                minetest.set_node(pos, {name = name})
+            end
+        end
+    end,
+})
 
 --
 -- Growing
