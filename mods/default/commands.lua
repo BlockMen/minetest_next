c1876a156c7407b3abb7eb32951afbecd61f73ee
LNJ <lnj@jahnson.de>
2015-11-20 20:58:46 +0100

    Deleted clear_inv command

--- mods/default/commands.lua
+++ mods/default/commands.lua
@@ -1,23 +1,3 @@
-minetest.register_chatcommand("clearinv", {
-	params = "<inventory>",
-	description = "Clears an entire inventory, \"main\" if unspecified, \"craft\" is another possible choice",
-	func = function(name, param)
-		local player = minetest.get_player_by_name(name)
-		local player_inv = player:get_inventory()
-		if not player then
-			minetest.log("error", "Unable to clear inventory, no player.")
-			return false, "Unable to clear inventory, no player."
-		end
-		if param == "" then
-			player_inv:set_list("main", {})
-			return true, "Inventory \"main\" cleared."
-		else
-			player_inv:set_list(param, {})
-			return true, "Inventory \"" .. param .. "\" cleared."
-		end
-	end,
-})
-
 minetest.register_chatcommand("whoami", {
         params = "",
         description = "Tells your name",
