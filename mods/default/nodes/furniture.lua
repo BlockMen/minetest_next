fc91c27d86eb85b535e29839b6bec32d9ca4eb5f
LNJ <lnj@jahnson.de>
2016-01-25 21:33:43 +0100

    Add table API

--- mods/default/nodes/furniture.lua
+++ mods/default/nodes/furniture.lua
@@ -1,1 +1,64 @@
+function default.register_table(itemstring, def)
+	
+	if not def.groups then
+		def.groups = {choppy = 2, flammable = 2, oddly_breakable_by_hand = 2, fuel = 6},
+	end
+	if not def.sounds then
+		def.sounds = default.node_sound_wood_defaults()
+	end
+	
+	minetest.register_node(itemstring, {
+		description = def.description,
+		tiles = {def.texture},
+		drawtype = "nodebox",
+		paramtype = "light",
+		groups = def.groups,
+		sounds = def.sounds,
+		node_box = {
+			type = "fixed",
+			fixed = {
+				{-0.5, 0.375, -0.5, 0.5, 0.5, 0.5},
+				{-0.1875, -0.5, -0.1875, 0.1875, 0.375, 0.1875},
+			}
+		}
+	})
 
+	minetest.register_craft({
+		output = itemstring .. " 2",
+		recipe = {
+			{def.material, def.material, def.material},
+			{      "",     def.material,      ""     },
+			{      "",     def.material,      ""     },
+		}
+	})
+end
+
+default.register_table("default:table_wood", {
+	description = "Wooden Table",
+	texture = "default_wood.png",
+	material = "default:wood"
+})
+
+default.register_table("default:table_junglewood", {
+	description = "Junglewood Table",
+	texture = "default_junglewood.png",
+	material = "default:junglewood"
+})
+
+default.register_table("default:table_pinewood", {
+	description = "Pine Wood Table",
+	texture = "default_pine_wood.png",
+	material = "default:pinewood"
+})
+
+default.register_table("default:table_acaciawood", {
+	description = "Acacia Wood Table",
+	texture = "default_acacia_wood.png",
+	material = "default:acacia_wood"
+})
+
+default.register_table("default:table_birchwood", {
+	description = "Birch Wood Table",
+	texture = "default_birch_wood.png",
+	material = "default:birch_wood"
+})
